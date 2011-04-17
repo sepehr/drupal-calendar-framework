76b10495791ce4512e9003c82894a909c4f06d65
Sepehr Lajevardi <me@sepehr.ws>
2011-04-17 23:17:23 +0430

    Added API file stub.

---
+++ calendar_systems.api.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * API provided by the Calendar Systems module.
+ *
+ * @todo
+ *   - Implement.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * @} End of "addtogroup hooks".
+ */
+
